c9ed5852e5a138babfb6d16740bdfd65f501ddca
Oladejo Olakareem <oladejoolakareem@Oladejos-MacBook-Pro.local>
2017-10-12 20:40:18 -0700

    Develop user sign in and All attributes of user :pencil:

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,5 @@
 class UsersController < ApplicationController
+
   def new
     @user = User.new
   end
@@ -6,15 +7,21 @@ class UsersController < ApplicationController
   def create
     @user = User.new(allowed_params)
     if @user.save
-      redirect_to root_url, notice: 'Thank you for signing up!'
+      session[:user_id] = @user.id
+      redirect_to user_path(@user)
     else
       render :new
     end
   end
 
+  def show
+    @user = User.find(params[:id])
+  end
+
+
   private
 
   def allowed_params
-    params.require(:user).permit(:email, :password_digest)
+    params.require(:user).permit(:email, :password, :password_confirmation)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   get 'sessions/new'
-
+  resources :sessions
   resources :users
+  get 'signup', to: 'users#new', as: 'signup'
+  get 'login', to: 'sessions#new', as: 'login'
+  get 'logout', to: 'sessions#destroy', as: 'logout'
+
+  root 'users#new'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
