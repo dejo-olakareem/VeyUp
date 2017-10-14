4a3473a9b4dab4e4edb1a8cc135ff20482e00dd0
Oladejo Olakareem <oladejoolakareem@Oladejos-MacBook-Pro.local>
2017-10-13 17:09:03 -0700

    Generate question model and migrated :pencil:

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171013224514) do
+ActiveRecord::Schema.define(version: 20171013230305) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "questions", force: :cascade do |t|
+    t.text "text"
+    t.integer "survey_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "surveys", force: :cascade do |t|
     t.string "name"
