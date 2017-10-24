fa77ce9faa715f00105eb9e7a89199af10bb2726
Oladejo Olakareem <oladejoolakareem@Oladejos-MacBook-Pro.local>
2017-10-23 18:53:44 -0700

    Refactored find_by to find_or_create_by for heroku because it was depreciated in rails 3 🚀

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -16,7 +16,7 @@ class SessionsController < ApplicationController
 
   def create_from_facebook
     auth =  request.env["omniauth.auth"]
-    user = User.find_by_provider_and_uid(auth["provider"], auth["uid"]) || User.create_with_omniauth(auth)
+    user = User.find_or_create_by(provider: auth["provider"],uid: auth["uid"]) || User.create_with_omniauth(auth)
     session[:user_id] = user.id
     redirect_to user_path(user), :notice => "Signed in!"
   end
