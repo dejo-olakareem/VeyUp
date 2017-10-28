1533ed48ae3424261087da19e004d4b571de995f
Oladejo Olakareem <oladejoolakareem@Oladejos-MacBook-Pro.local>
2017-10-27 17:59:47 -0700

    Delete unwanted code

--- app/assets/javascripts/header.js
+++ app/assets/javascripts/header.js
@@ -1,9 +1,5 @@
 $(document).on("turbolinks:load", function(){
   goin()
-  // checked()
-  example()
-  // clickoncoco()
-
 });
 
 var goin = function(){
@@ -18,29 +14,3 @@ var goin = function(){
   })
 }
 
-
-// var checked = function(){
-//   var i = 0
-//   var rating = $('.rator').first().text()
-//   var rate = parseFloat(rating);
-//   while(i < rate){
-//     $('#' + i).addClass("checked")
-//     i++
-//   }
-// }
-
-
-var example = function(){
-var rating = $('.rator')
-for (var index = 0; index < rating.length; index++) {
-  console.log(rating[index].innerHTML)
-  //   $('#' + i).addClass("checked")
-
-  // var i = 0
-  // while(i < rating[index].innerHTML){
-  //     console.log("add class to element")
-  //   $('#' + i).addClass("checked")
-  //   i++
-  // }
-}
-}
