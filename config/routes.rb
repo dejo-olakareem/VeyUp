168a242dbbf003bf835f5f8f72d869e98ac9ca3e
Oladejo Olakareem <oladejoolakareem@Oladejos-MacBook-Pro.local>
2017-11-09 11:30:59 -0800

    Change home view to Question index

--- config/routes.rb
+++ config/routes.rb
@@ -33,6 +33,6 @@ Rails.application.routes.draw do
   get 'signup', to: 'users#new'
   get 'login', to: 'sessions#new'
   get 'logout', to: 'sessions#destroy'
-  root 'businesses#new'
+  root 'questions#index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
