9dc49a30b0b6d31cda082f795dc8d1b36977b783
Oladejo Olakareem <oladejoolakareem@Oladejos-MacBook-Pro.local>
2017-11-12 16:39:46 -0800

    Validate name and email and also presence

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,6 +15,8 @@ class User < ApplicationRecord
   has_many :following, through: :active_relationships, source: :followed
   has_many :followers, through: :passive_relationships, source: :follower
 
+  validates :name,:email, presence: true
+
   # Follows a user.
   def follow(other_user)
     following << other_user
